ba4f4ed546425eb11ffaaee4eade4302f4de7c33
coelho-glith <coelho.soldado7@gmail.com>
2025-03-06 17:31:45 -0300

    Calculadora 2.0

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("------------------------------------");
             Console.WriteLine("Calculadora Tabajara 2025");
+            Console.WriteLine("------------------------------------");
+
+            Console.WriteLine("1 - Somar");
+            Console.WriteLine("2 - Subtrair");
+            Console.WriteLine("S - Sair");
+
+            Console.WriteLine();
+            Console.Write("Escolha uma opção: ");
+
+            string opcao = Console.ReadLine();
+
+            string opcaoValidada = opcao.ToUpper();
+
+            if(opcao == "S")
+            {
+                return;
+            }
 
             Console.Write("Digite o primeiro número:");
             int primeiroNumero = Convert.ToInt32(Console.ReadLine());
@@ -12,7 +30,16 @@
             Console.Write("Digite o segundo número:");
             int segundoNumero = Convert.ToInt32(Console.ReadLine());
 
-            int resultado = primeiroNumero + segundoNumero;
+            int resultado = 0;
+
+            if (opcao == "1")
+            {
+                resultado = primeiroNumero + segundoNumero;
+            }
+            else 
+            {
+                resultado = primeiroNumero - segundoNumero;
+            }
 
             Console.WriteLine(resultado);
 
