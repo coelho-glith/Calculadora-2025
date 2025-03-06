3eaf3b714484d26e7894579cbf9a14b19da80805
coelho-glith <coelho.soldado7@gmail.com>
2025-03-06 17:05:56 -0300

    Add project files.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora Tabajara 2025");
+
+            Console.Write("Digite o primeiro número:");
+            int primeiroNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo número:");
+            int segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = primeiroNumero + segundoNumero;
+
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+
+        }
+    }
+}
